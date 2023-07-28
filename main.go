838477d3fe74271b401459714246a14df24c4cae
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-07-28 23:37:43 +0100

    receive message from whatsapp

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/twilio/twilio-go"
+	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
+	"log"
+	"os"
+	"strings"
+)
+
+type TwillioMessage struct {
+	Body string
+	From string
+}
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Post("/twilio/webhook", func(c *fiber.Ctx) error {
+		message := TwillioMessage{}
+		err := c.BodyParser(&message)
+
+		if err != nil {
+			log.Fatal(err)
+			return c.JSON(&fiber.Map{"success": true})
+		}
+
+		log.Printf("received message from %s\n", message.From)
+
+		SendWhatsappMessage(message.From, "You said: "+message.Body)
+		return c.JSON(&fiber.Map{"success": true})
+	})
+
+	app.Listen(":3000")
+}
+
+func SendWhatsappMessage(to string, body string) {
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: os.Getenv("TWILLIO_ACCOUND_SID"),
+		Password: os.Getenv("TWILLIO_AUTH_TOKEN"),
+	})
+
+	params := twilioApi.CreateMessageParams{}
+	params.SetFrom("whatsapp:" + os.Getenv("TWILLIO_PHONE"))
+	params.SetBody(body)
+
+	if strings.HasPrefix(to, "whatsapp:") {
+		params.SetTo(to)
+	} else {
+		params.SetTo("whatsapp:" + to)
+	}
+
+	_, err := client.Api.CreateMessage(&params)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
